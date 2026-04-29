7f735e015c9540be0b021ce4fe2d024da3bdb2de
SamX20 <127461358+SamX20@users.noreply.github.com>
2026-04-29 03:50:39 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://fsmueisfdtuyljvpwlkj.supabase.co';
-const supabaseAnon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_OYvqHn-a05IyrCyBQEPx9A_A7CBprpt';
-const supabaseService = process.env.SUPABASE_SERVICE_ROLE_KEY || 'sb_secret_ukM7KMdOHLW3_K0RbmZ6kg_gwBqoV3B';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseService = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnon);
 export const supabaseAdmin = createClient(supabaseUrl, supabaseService);
